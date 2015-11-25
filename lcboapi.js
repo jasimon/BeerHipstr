a88490d0f72b988c74eb048e4df6dd9deccd1c38
Jacob <jacob.simon01@gmail.com>
2015-11-24 23:57:22 -0500

    checking inventory before showing the beers, final cleanup/refactor stuff

--- lcboapi.js
+++ lcboapi.js
@@ -1,9 +1,13 @@
 var https = require('https');
 var fs = require('fs');
+var Q = require('q');
 module.exports = (function() {
   const DATA_FILEPATH = 'data/used_beers.txt';
   const BEER_API_PATH = '/products?store_id=511&q=beer&where_not=is_dead,is_discontinued';
+  const INVENTORY_API_PATH_START = '/stores/511/products/';
+  const INVENTORY_API_PATH_END = '/inventory';
   const AUTH_TOKEN = 'Token MDo5MDRjMTU0NC05MjU1LTExZTUtYjBkOS1kMzc3MTBkZTdhZDA6bDJMV0d0QTI5N0R1SWl3bGVMSmZwUE05cTFZdGxaUWZTU2FH';
+  const PER_PAGE = 20; //Default from API
   
   var options = {
     hostname: 'lcboapi.com',
@@ -14,6 +18,7 @@ module.exports = (function() {
   };
 
   var beers = []; 
+  var beers_in_stock = [];
   var used_names = [];
   var requestPage = 0;
 
@@ -22,13 +27,14 @@ module.exports = (function() {
       return console.log(err);
     }
     used_names = data.split('\n');
+
   });
 
   function getBeer(callback) {
-    if (beers.length == 0) {
+    if (beers_in_stock.length == 0) {
       requestPage++;
       options.path = BEER_API_PATH + '&page=' + requestPage;
-      var req = https.get(options, function(response) {
+      https.get(options, function(response) {
         var output = '';
         response.on('data', function(chunk) {
           output += chunk;
@@ -40,7 +46,15 @@ module.exports = (function() {
             return used_names.indexOf(item.name) < 0;
           });
           if(beers.length > 0) {
-            selectBeer(callback);
+            
+          checkQuantity()
+            .then(function() {
+              callback(selectBeer());
+            }, function(err) {
+              if (err === "ERR_NO_MATCHES") {
+                getBeer(callback);
+              }
+            });
           } else {
             getBeer(callback);
           }
@@ -49,26 +63,60 @@ module.exports = (function() {
         console.log(e);
       });
     } else {
-      selectBeer(callback);
+      callback(selectBeer());
     }
   }
 
-  function selectBeer(callback) {
-    var selected = beers.pop();
+  function selectBeer() {
+    var selected = beers_in_stock.pop();
     used_names.push(selected.name);
     //remove any existing beers with the same name
-    beers = beers.filter(function(item) {
+    beers_in_stock = beers_in_stock.filter(function(item) {
       return used_names.indexOf(item.name) < 0;
     });
     fs.writeFile(DATA_FILEPATH, used_names.join('\n'), function(err) {
       if (err) {
         return console.log(err);
       }
-      console.log('written');
+      console.log('Wrote names to file');
     });
     //send the selected beer to the callback
-    callback(selected);
+    return selected;
 
+  }
+
+  function checkQuantity() {
+      //use promises so the first one that has stock is returned
+      var deferred = Q.defer();
+      var failCount = 0;
+      beers.forEach(function(item) {
+      options.path = INVENTORY_API_PATH_START + item.id + INVENTORY_API_PATH_END;
+      https.get(options, function(response) {
+        var output = '';
+        response.on('data', function(chunk) {
+          output += chunk;
+        });
+
+        response.on('end', function(chunk) {
+          var resp = JSON.parse(output);
+          if (resp.result.quantity > 0) {
+            beers_in_stock.push(item);
+            //no guaranteed order but will only resolve once
+            deferred.resolve();
+          } else {
+            failCount++;
+            if (failCount == beers.length) {
+              deferred.reject('ERR_NO_MATCHES');
+            }
+          }
+        })
+      }).on('error', function(e) {
+        console.log(e);
+        deferred.reject(e);
+      })
+
+    });
+    return deferred.promise;
   }
 
   return {
